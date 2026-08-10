fbeae1ff40a62b0e8de10fceabae067545927dbc
Christian John Roa <roacj.cr@gmail.com>
2026-08-10 17:33:57 +0800

    Refactor user and authentication folder structure

---
+++ src/lib/services/user/auth.service.ts
@@ -0,0 +1,56 @@
+import { apiClient } from "../axiosClient";
+
+export interface LoginPayload {
+  username: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  success: boolean;
+  isVerified: boolean;
+  email: string;
+  token?: string;
+}
+
+export interface ForgotPasswordPayload{
+  email: string;
+}
+
+export interface ForgotPasswordResponse{
+  success: boolean;
+  message: string;
+}
+
+export interface ResetPasswordPayload{
+  token: string;
+  newPassword: string;
+}
+
+export interface ResetPasswordResponse{
+  success: boolean;
+  message: string;
+}
+
+export interface VerifyEmailPayload {
+  token: string;
+}
+
+export interface VerifyEmailResponse {
+  success: boolean;
+  message: string;
+}
+
+export const authApiService = {
+  login: async (credentials: LoginPayload): Promise<LoginResponse> => {
+    return apiClient.post("/auth/login", credentials);
+  },
+  forgotPassword: async (payload: ForgotPasswordPayload): Promise<ForgotPasswordResponse> => {
+    return apiClient.post("/auth/forgot-password", payload);
+  },
+  resetPassword: async (payload: ResetPasswordPayload): Promise<ResetPasswordResponse> => {
+    return apiClient.post("/auth/reset-password", payload);
+  },
+  verifyEmail: async (payload: VerifyEmailPayload): Promise<VerifyEmailResponse> => {
+    return apiClient.get(`/api/auth/verifyEmail?token=${payload.token}`);
+  },
+};
