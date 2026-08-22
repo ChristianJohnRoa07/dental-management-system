09f44f2f35324d876064292702ab4b9c56477daa
Christian John Roa <roacj.cr@gmail.com>
2026-08-22 18:08:18 +0800

    Include

--- src/lib/services/procedure/procedure.service.ts
+++ src/lib/services/procedure/procedure.service.ts
@@ -17,9 +17,28 @@ export interface Response {
   message?: string;
 }
 
+export interface CreateProcedurePayload {
+  name: string;
+  description: string;
+  price: number;
+  userId: string;
+}
+
 export const procedureApiService = {
   getProcedures: async (token?: string): Promise<Response> => {
     return apiClient.get("/procedures", {
+      headers: token ? { Authorization: `Bearer ${token}` } : {},
+    });
+  },
+  
+  createProcedure: async (
+    payload: CreateProcedurePayload,
+    token?: string
+  ): Promise<Response> => {
+
+    console.log("API Payload: ",payload);
+
+    return apiClient.post("/procedures", payload, {
       headers: token ? { Authorization: `Bearer ${token}` } : {},
     });
   },
