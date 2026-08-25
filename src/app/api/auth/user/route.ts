0caffbd874eb08400752d3a6ae32a4b5c09375ae
Christian John Roa <roacj.cr@gmail.com>
2026-08-25 09:21:29 +0800

    Remove access token inclusion on fetch user api

--- src/app/api/auth/user/route.ts
+++ src/app/api/auth/user/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
 import { getEncryptedUserCookie } from "@/lib/hooks/api/authCookies";
+import { UserSession } from "@/interface/user/userInSession";
 
 export async function GET() {
   try {
-    const user = await getEncryptedUserCookie();
+    const user = (await getEncryptedUserCookie()) as UserSession | null;
 
     if (!user) {
       return NextResponse.json({
@@ -13,9 +14,19 @@ export async function GET() {
       });
     }
 
+    const sanitizedUser = {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      role: user.role,
+      isVerified: user.isVerified
+    };
+
     return NextResponse.json({
       status: "success",
-      data: user,
+      data: sanitizedUser,
     });
   } catch (error) {
     return NextResponse.json(
