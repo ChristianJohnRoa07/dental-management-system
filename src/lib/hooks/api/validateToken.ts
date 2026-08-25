cf7781eeacb4ee4c5dc6d64432b3eb279c4863fb
Christian John Roa <roacj.cr@gmail.com>
2026-08-25 09:28:45 +0800

    Implement JWT payload interface

--- src/lib/hooks/api/validateToken.ts
+++ src/lib/hooks/api/validateToken.ts
@@ -1,11 +1,41 @@
+import { jwtVerify } from "jose";
 import db from "@/lib/db";
+import { CustomJwtPayload } from "@/interface/user/JWTPayload";
 
-export async function isTokenBlacklisted(token: string): Promise<boolean> {
+const secretKey = process.env.JWT_SECRET;
+if (!secretKey) {
+  throw new Error("JWT_SECRET environment variable is missing.");
+}
+
+const JWT_SECRET = new TextEncoder().encode(secretKey);
+
+async function isTokenBlacklisted(token: string): Promise<boolean> {
   if (!token) return false;
 
-  const blacklisted = await db.tokenBlacklist.findUnique({
-    where: { token },
-  });
+  try {
+    const blacklisted = await db.tokenBlacklist.findUnique({
+      where: { token },
+    });
+    return !!blacklisted;
+  } catch (error) {
+    return false;
+  }
+}
 
-  return !!blacklisted;
+export async function validateToken(
+  token: string
+): Promise<CustomJwtPayload | null> {
+  if (!token) return null;
+
+  try {
+    const blacklisted = await isTokenBlacklisted(token);
+    if (blacklisted) {
+      return null;
+    }
+
+    const { payload } = await jwtVerify(token, JWT_SECRET);
+    return payload as CustomJwtPayload;
+  } catch (error: any) {
+    return null;
+  }
 }
