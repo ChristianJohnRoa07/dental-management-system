7fe85b34b2c49776188214869664c7dca1972630
Christian John Roa <roacj.cr@gmail.com>
2026-08-25 09:27:57 +0800

    Implement user session interface

--- src/lib/hooks/api/authCookies.ts
+++ src/lib/hooks/api/authCookies.ts
@@ -1,19 +1,20 @@
 import { cookies } from "next/headers";
 import { encryptObject, decryptObject } from "@/lib/hooks/ui/encrpytDecrypt";
 import { SESSION_DURATION_SECONDS, USER_COOKIE_NAME } from "@/lib/constants";
+import { UserSession } from "@/interface/user/userInSession";
 
 /**
  * Encrypts user data and sets it in an HTTP-only secure cookie.
  * (Call this in Next.js Server Actions, Route Handlers, or SSR context)
  */
-export async function setEncryptedUserCookie<T>(userData: T): Promise<void> {
-  const encryptedData = encryptObject(userData);
-
+export async function setEncryptedUserCookie(userSession: any): Promise<void> {
   const cookieStore = await cookies();
+  const encryptedData = encryptObject(userSession);
+
   cookieStore.set(USER_COOKIE_NAME, encryptedData, {
     httpOnly: true, // Prevents client-side JS access (XSS protection)
     secure: process.env.NODE_ENV === "production", // Ensures cookie is sent over HTTPS
-    sameSite: "lax", // CSRF protection
+    sameSite: "strict", // CSRF protection
     maxAge: SESSION_DURATION_SECONDS,
     path: "/",
   });
@@ -22,16 +23,16 @@ export async function setEncryptedUserCookie<T>(userData: T): Promise<void> {
 /**
  * Retrieves and decrypts user data from the HTTP-only cookie.
  */
-export async function getEncryptedUserCookie<T>(): Promise<T | null> {
+export async function getEncryptedUserCookie(): Promise<UserSession | null> {
   try {
     const cookieStore = await cookies();
-    const encryptedCookie = cookieStore.get(USER_COOKIE_NAME);
+    const encryptedCookie = cookieStore.get(USER_COOKIE_NAME)?.value;
 
-    if (!encryptedCookie?.value) {
+    if (!encryptedCookie) {
       return null;
     }
 
-    return decryptObject<T>(encryptedCookie.value);
+    return decryptObject(encryptedCookie);
   } catch (error) {
     console.error("Failed to decrypt user cookie:", error);
     return null;
